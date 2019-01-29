6f6337e87ed72fef7bf6b33f65d702851cc2d22e
echoman12 <45900323+echoman12@users.noreply.github.com>
2019-01-29 14:14:01 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '-'
+const prefix = '$'
  
  
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`-bc`,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(`$bc`,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
